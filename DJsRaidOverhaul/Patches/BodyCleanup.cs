b46c1b22d56e44c99fbd9b4dbdb659b80c526f57
DJ <147062692+KillerDJLang@users.noreply.github.com>
2024-01-06 20:44:09 -0500

    Add files via upload

---
+++ DJsRaidOverhaul/Patches/BodyCleanup.cs
@@ -0,0 +1,61 @@
+﻿using EFT;
+using UnityEngine;
+using Comfort.Common;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace DJsRaidOverhaul.Patches
+{
+    public class BodyCleanup : MonoBehaviour
+    {
+        private bool _MaidOnStandby = false;
+
+        void Update()
+        {
+            if (!Ready() || !Plugin.EnableClean.Value)
+            {
+                return;
+            }
+
+            if (!_MaidOnStandby)
+            {
+                StaticManager.Instance.StartCoroutine(StartClean());
+                _MaidOnStandby = true;
+            }
+        }
+
+        private IEnumerator StartClean()
+        {
+            yield return new WaitForSeconds(Plugin.TimeToClean.Value * 60f);
+
+            if (Gameworld != null && Gameworld.AllAlivePlayersList != null && Gameworld.AllAlivePlayersList.Count > 0 && !(Myplayer is HideoutPlayer))
+            {
+                Task.Delay(10000);
+                foreach (BotOwner bot in FindObjectsOfType<BotOwner>())
+                {
+                    if (!bot.HealthController.IsAlive && UnityEngine.Vector3.Distance(Myplayer.Transform.position, bot.Transform.position) >= Plugin.DistToClean.Value)
+                    {
+                        bot.gameObject.SetActive(false);
+                    }
+                }
+            }
+
+            else
+            {
+                _MaidOnStandby = false;
+                yield break;
+            }
+
+            _MaidOnStandby = false;
+            yield break;
+        }
+
+        public bool Ready() => Gameworld != null && Gameworld.AllAlivePlayersList != null && Gameworld.AllAlivePlayersList.Count > 0 && !(Myplayer is HideoutPlayer);
+
+        Player Myplayer
+        { get => Gameworld.AllAlivePlayersList[0]; }
+
+        GameWorld Gameworld
+        { get => Singleton<GameWorld>.Instance; }
+    }
+}
---
+++ DJsRaidOverhaul/Patches/GameWorldDisposePatch.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Aki.Reflection.Patching;
+using EFT;
+
+namespace DJsRaidOverhaul.Patches
+{
+    public class GameWorldDisposePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(GameWorld).GetMethod(nameof(GameWorld.Dispose));
+        }
+
+        [PatchPrefix]
+        private static void PatchPrefix()
+        {
+            Plugin.Controllers.Clear();
+        }
+    }
+}
